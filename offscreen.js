77f0353535aed8c35505163d5ef5001ef4de970b
Sergei Sobolev <s.sobolev@calendaria.ai>
2024-05-04 21:48:28 +0500

    service worker

--- offscreen.js
+++ offscreen.js
@@ -1,19 +1,4 @@
-// Copyright 2023 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 chrome.runtime.onMessage.addListener(async (message) => {
-  console.log('onMessage:');
   if (message.target === 'offscreen') {
     switch (message.type) {
       case 'start-recording':
@@ -28,29 +13,26 @@ chrome.runtime.onMessage.addListener(async (message) => {
   }
 });
 
+let interval;
 let recorder;
 let data = [];
-
 let config = {};
- 
+
+const mimeType = 'audio/webm; codecs=opus';
 
 // Function to fetch and parse the config file
-function loadConfig() 
-{
+function loadConfig() {
   fetch('config.json')
-      .then(response => response.json())
-      .then(data => {
-          console.log('Configuration loaded:', data);
-          config = data;
-      })
-      .catch(error => console.error('Error loading configuration:', error));
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Configuration loaded:', data);
+      config = data;
+    })
+    .catch((error) => console.error('Error loading configuration:', error));
 }
 
 async function startRecording(streamId) {
-  console.log('startRecording:', streamId);
   loadConfig();
-  const currentDate = new Date().toLocaleString('en-US', { timeZone: 'UTC' });
-  const transcript_title="Transcript "+ currentDate;
   if (recorder?.state === 'recording') {
     throw new Error('Called startRecording while recording is in progress.');
   }
@@ -59,9 +41,9 @@ async function startRecording(streamId) {
     audio: {
       mandatory: {
         chromeMediaSource: 'tab',
-        chromeMediaSourceId: streamId
-      }
-    }
+        chromeMediaSourceId: streamId,
+      },
+    },
   });
 
   // Continue to play the captured audio to the user.
@@ -81,67 +63,77 @@ async function startRecording(streamId) {
   //   mixedDest.stream.getTracks()[0]
   // ]);
 
+  const currentDate = new Date().toLocaleString('en-US', { timeZone: 'UTC' });
+  const transcript_title = 'Transcript ' + currentDate;
+  if (recorder?.state === 'recording') {
+    throw new Error('Called startRecording while recording is in progress.');
+  }
+
   // Start recording.
-  recorder = new MediaRecorder(media, { mimeType: 'audio/webm; codecs=opus' });
-  recorder.ondataavailable = (event) => data.push(event.data);
+  recorder = new MediaRecorder(media, {
+    mimeType,
+    audioBitsPerSecond: 16000,
+  });
+  recorder.ondataavailable = (event) => {
+    console.log('sending chunk', recorder.audioBitsPerSecond);
+    data.push(event.data);
+    const reader = new FileReader();
+    reader.readAsDataURL(event.data);
+    reader.onloadend = function () {
+      // let bytes = new Uint8Array(reader.result);
+      // var blob = btoa(
+      //   bytes.reduce((data, byte) => data + String.fromCharCode(byte), ''),
+      // );
+      const blob = reader.result.replace(
+        'data:audio/webm;codecs=opus;base64,',
+        '',
+      );
+      chrome.runtime.sendMessage({
+        contentScriptQuery: 'upload-chunk',
+        data: {
+          blob,
+          // stop: interval === undefined,
+        },
+      });
+    };
+  };
   recorder.onstop = () => {
-    const blob = new Blob(data, { type: 'audio/ogg; codecs=opus' });
+    recorder = undefined;
+
+    // if (interval) {
+    //   clearInterval(interval);
+    //   interval = undefined;
+    // }
+
+    const blob = new Blob(data);
     const url = URL.createObjectURL(blob);
-  
+
     // Create a new anchor element
     const a = document.createElement('a');
-    
+
     // Set the href and download attributes for the anchor element
     a.href = url;
-    a.download = transcript_title+'.ogg'; // You can name the file here
-    
+    a.download = transcript_title + '.webm'; // You can name the file here
+
     // Append the anchor to the document
     document.body.appendChild(a);
-    
+
     // Trigger a click on the anchor to start download
     a.click();
-    
+
     // Clean up by removing the anchor element and revoking the blob URL
     document.body.removeChild(a);
     URL.revokeObjectURL(url);
 
-    console.log("Audio blob saved");
-
-    // Step 1: Convert Blob to File
-    const audioFile = new File([blob], "audio.ogg; codecs=opus", { type: 'audio/ogg; codecs=opus' });
-
-    console.log("Audio file prepared");
-
-    // Step 2: Prepare FormData
-    const formData = new FormData();
-    formData.append("audio", audioFile);
-
-    console.log("Form data prepared");
-
-    // Step 3: Set up HTTP Request
-    fetch(config.SBER_API_ENDPOINT, {
-        method: 'POST',
-        headers: {
-            'Authorization': 'Bearer ' + config.SBER_ACCESS_TOKEN,
-            'Content-Type': 'multipart/form-data'
-        },
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Step 4: Handle Response
-        console.log("Transcription: ", data); // Display or process the transcription
-    })
-    .catch(error => {
-        console.error("Error: ", error);
-    });
-
-
-    // Clear state ready for next recording
-    recorder = undefined;
-    data = [];
+    console.log('Audio blob saved');
   };
-  recorder.start();
+  recorder.start(1000);
+
+  // interval = setInterval(() => {
+  //   recorder.requestData();
+  // }, 1000);
+
+  console.log('Recording started.', mimeType, recorder.audioBitsPerSecond);
 
   // Record the current state in the URL. This provides a very low-bandwidth
   // way of communicating with the service worker (the service worker can check
@@ -167,4 +159,3 @@ async function stopRecording() {
   // make sure the browser keeps the Object URL we create (see above) and to
   // keep the sample fairly simple to follow.
 }
-
--- service-worker.js
+++ service-worker.js
@@ -12,12 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+let startRec = true;
+let stopRec = false;
+
 chrome.action.onClicked.addListener(async (tab) => {
   const existingContexts = await chrome.runtime.getContexts({});
   let recording = false;
 
   const offscreenDocument = existingContexts.find(
-    (c) => c.contextType === 'OFFSCREEN_DOCUMENT'
+    (c) => c.contextType === 'OFFSCREEN_DOCUMENT',
   );
 
   // If an offscreen document is not already open, create one.
@@ -26,16 +29,17 @@ chrome.action.onClicked.addListener(async (tab) => {
     await chrome.offscreen.createDocument({
       url: 'offscreen.html',
       reasons: ['USER_MEDIA'],
-      justification: 'Recording from chrome.tabCapture API'
+      justification: 'Recording from chrome.tabCapture API',
     });
   } else {
     recording = offscreenDocument.documentUrl.endsWith('#recording');
   }
 
   if (recording) {
+    stopRec = true;
     chrome.runtime.sendMessage({
       type: 'stop-recording',
-      target: 'offscreen'
+      target: 'offscreen',
     });
     chrome.action.setIcon({ path: 'icons/not-recording.png' });
     return;
@@ -43,15 +47,63 @@ chrome.action.onClicked.addListener(async (tab) => {
 
   // Get a MediaStream for the active tab.
   const streamId = await chrome.tabCapture.getMediaStreamId({
-    targetTabId: tab.id
+    targetTabId: tab.id,
   });
 
   // Send the stream ID to the offscreen document to start recording.
   chrome.runtime.sendMessage({
     type: 'start-recording',
     target: 'offscreen',
-    data: streamId
+    data: streamId,
   });
 
   chrome.action.setIcon({ path: '/icons/recording.png' });
+});
+
+const mimeType = 'audio/webm; codecs=pcm';
+const LOCAL_SERVER_ENDPOINT = 'http://localhost:8021/v1/transcriber';
+
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  switch (message.contentScriptQuery) {
+    case 'upload-chunk':
+      // const blob = await fetch(message.data.blob).then((res) => res.blob());
+
+      // const chunk = new Uint8Array(blob);
+      // const audioFile = new File([blob], 'file.webm', {
+      //   type: mimeType,
+      // });
+
+      let endpoint = 'chunk';
+      if (stopRec) {
+        endpoint = 'stop';
+        startRec = true;
+        stopRec = false;
+      } else if (startRec) {
+        endpoint = 'start';
+        startRec = false;
+        console.log('Start recording');
+      }
+
+      fetch(`${LOCAL_SERVER_ENDPOINT}/upload-${endpoint}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // 'Content-Type': mimeType,
+          // 'Authorization': 'Bearer ' + config.SBER_ACCESS_TOKEN,
+          // 'X-Framerate': MediaRecorder.audioBitsPerSecond,
+        },
+        body: JSON.stringify({
+          chunk: message.data.blob,
+        }),
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log('Transcription: ', data);
+        })
+        .catch(console.error);
+      break;
+  }
+  return true;
 });
